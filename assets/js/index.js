b5b6ac8edc84d3566c8ae267c974e673e228bc6e
nicolai2 <nicolaj.pavlov@gmail.com>
2024-10-19 00:17:17 -0300

    Se arregla el archivo JS y css Se arreglan las funciones por procesos y métodos más eficaces. Se añade imagen de computador

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,19 @@
-const precio = 400000;
-let total = document.querySelector(".valor-total");
-total.innerHTML = precio;
+const precioBase = 400000;
+let precioTotal = document.querySelector(".valor-total");
+let contador = 0;
+let contadorTotal = document.querySelector(".cantidad");
 
-function clicksMas() {
-  click = document.querySelector(".cantidad");
-  contador = parseInt(click.innerHTML);
-  contador = contador + 1;
-  click.innerHTML = contador;
-  total.innerHTML = precio * contador;
+function clickMas() {
+  contador++;
+  contadorTotal.innerHTML = contador;
+  precioTotal.innerHTML = precioBase * contador;
 }
 
-function clicksMenos() {
-  click = document.querySelector(".cantidad");
-  contador = parseInt(click.innerHTML);
-  contador = contador - 1;
-  click.innerHTML = contador;
-  total.innerHTML = precio * contador;
+function clickMenos() {
+  contador--;
   if (contador < 0) {
-    click.innerHTML = 0;
-    total.innerHTML = 0;
+    contador = 0;
   }
+  contadorTotal.innerHTML = contador;
+  precioTotal.innerHTML = precioBase * contador;
 }
-
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio;
